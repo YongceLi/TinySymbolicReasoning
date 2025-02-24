fb6847910dc4f74d6381f00c9d261b4a8f95b0ca
YongceLi <100247584+YongceLi@users.noreply.github.com>
2025-02-24 11:52:49 -0800

    updated sft dataset

---
+++ format_training_dataset.py
@@ -0,0 +1,56 @@
+import json
+
+with open("prompt.txt", "r") as f:
+    prompt = f.read()
+
+content_template = """
+We can represent the problem as the following symbolic logic.
+<problem_logic>
+
+Let's use symbolic reasoning to solve the problem.
+<think><symbolic_reasoning></think>
+<answer><solutions></answer>
+"""
+
+with open("train.jsonl", "r") as f:
+    train_data = f.readlines()
+
+sft_train = []
+for data in train_data:
+    data = json.loads(data)
+    curr_data = {}
+    curr_data["messages"] = []
+    curr_data["messages"].append(
+        {
+            "role": "system",
+            "content": "You are a strong symbolic reasoning model."
+        }
+    )
+    prompt_text = prompt.replace("<statements>", '\n'.join(data["problem"])).replace("<num-characters>", str(data["metadata"]["num_characters"]))
+    curr_data["messages"].append(
+        {
+            "role": "user",
+            "content": prompt_text
+        }
+    )
+    problem_logic = '\n'.join(data["problem_logic"])
+    symbolic_reasoning = '\n'.join([f"step {i + 1}: " + data["symbolic_reasoning"].split("\n")[i] for i in range(len(data["symbolic_reasoning"].split("\n")))])
+    solutions = ""
+    for key in data["solutions"][0]:
+        if data["solutions"][0][key]:
+            solutions += f"{key}: truth-teller\n"
+        else:
+            solutions += f"{key}: liar\n"
+    content = content_template.replace("<problem_logic>", problem_logic).replace("<symbolic_reasoning>", symbolic_reasoning).replace("<solutions>", solutions)
+    curr_data["messages"].append(
+        {
+            "role": "assistant",
+            "content": content
+        }
+    )
+    sft_train.append(curr_data)
+
+with open("sft_train.jsonl", "w") as f:
+    for item in sft_train:
+        json_str = json.dumps(item, indent=2)
+        f.write(json_str + "\n")
